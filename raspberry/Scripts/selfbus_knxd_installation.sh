95320c65e428cc5dc7b6ed6f9106e2ade8b01ee5
Doumanix <christian-balzer@gmx.de>
2018-01-09 20:10:11 +0100

    changed line breaks

--- raspberry/Scripts/selfbus_knxd_installation.sh
+++ raspberry/Scripts/selfbus_knxd_installation.sh
@@ -1,6 +1,4 @@
-﻿#!/bin/sh
-
-# Copyright (c) 2018 Christian Balzer <christian-balzer@gmx.de>
+﻿# Copyright (c) 2018 Christian Balzer <christian-balzer@gmx.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
